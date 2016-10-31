f5d79e24e4184776f13a466a279fca068ea60b6d
brown2206 <brown.angelo.jr@gmail.com>
2016-10-31 16:36:43 -0400

    set up controllers router config

--- app.js
+++ app.js
@@ -1,1 +1,32 @@
-angular.module("doc", ["ui.router"])
+angular
+  .module("doc", [
+    "ui.router",
+    "ngResource"
+])
+.config([
+  "$stateProvider",
+  RouterFunction
+])
+.controller("DoctorIndexController", [
+  DoctorIndexControllerFunction
+])
+.controller("DoctorShowController", [
+  "$stateParams",
+  DoctorShowControllerFunction
+])
+
+function RouterFunction($stateProvider){
+  $stateProvider
+    .state("doctorIndex", {
+      url: "/doctors",
+      templateUrl: "js/ng-views/index.html",
+      controller: "DoctorIndexController",
+      controllerAs: "vm"
+    })
+    .state("doctorShow", {
+      url: "/doctors/:id",
+      templateUrl: "js/ng-views/show.html",
+      controller: "DoctorShowController",
+      controllerAs: "vm"
+    })
+}
